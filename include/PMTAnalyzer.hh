692e3f6401f4581c2feaa294a6c998afc41e1627
Stephane Zsoldos <stephane.zsoldos@gmail.com>
2018-07-18 23:26:13 +0100

    Add PMTAnalyzer class def

---
+++ include/PMTAnalyzer.hh
@@ -0,0 +1,12 @@
+//
+// Created by zsoldos on 18/07/18.
+//
+
+#ifndef PMTANALYSIS_PMTANALYZER_HH
+#define PMTANALYSIS_PMTANALYZER_HH
+
+class PMTAnalyzer {
+
+};
+
+#endif //PMTANALYSIS_PMTANALYZER_HH
---
+++ src/PMTAnalyzer.cc
@@ -0,0 +1,5 @@
+//
+// Created by zsoldos on 18/07/18.
+//
+
+#include "PMTAnalyzer.hh"
